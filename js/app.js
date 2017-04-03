62f784e64ae4f080df16cebbfc5031ff8af7961f
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-02 21:53:41 -0700

    added correct js location

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
+  alert( "Handler for .click() called." );
+  console.log( "button.click() called." );
+    });
   // all code to manipulate the DOM
   // goes inside this function
 
-});
+
+// });
+// $(".button1").prev("a.ui-btn").unbind().click( function() {  $( "h3.center-block" ).text(" ");  } )
+// $(".button1").click( function() {  $( "h3.center-block" ).text(" ");  } )
