d0e5de608d17be0e00c7991cc42203f260da3e48
MJ-sfo <michaeljlaird@yahoo.com>
2017-04-02 23:55:50 -0700

    buttons to choose X or Y

--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
 // wait for the DOM to finish loading
 $(document).ready(function() {
-  alert( "Handler for .click() called." );
-  console.log( "button.click() called." );
-    });
+  // alert( "Handler for .click() called." );
+  // console.log( "button.click() called." );
+  $(".button1").click( function() {
+    $( "h3.center-block" ).text(" ");
+  } );
+
+  $(".buttonX").click( function() {
+    var choose = "X";
+    //  alert( "You are choosing " + choose );
+    //  console.log( choose +" chosen." );
+     $('.pleasechoosespan').text('You are choosing ' + choose);
+    });
+
+    $(".buttonY").click( function() {
+      var choose = "Y";
+       $('.pleasechoosespan').text('You are choosing ' + choose);
+      });
+
+    });   // this is for all document
+
+
   // all code to manipulate the DOM
   // goes inside this function
-
-
-// });
-// $(".button1").prev("a.ui-btn").unbind().click( function() {  $( "h3.center-block" ).text(" ");  } )
-// $(".button1").click( function() {  $( "h3.center-block" ).text(" ");  } )
